e87507374f46c3f5668780b49fb5718e616e056c
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-17 17:36:08 +0200

    opcion.CoinInfo

--- src/Hooks/useCriptomoneda.js
+++ src/Hooks/useCriptomoneda.js
@@ -26,9 +26,9 @@ const useCriptomoneda = (label, stateInicial, opciones) => {
                 value={state}
             >
                 <option value="">--Seleccione moneda--</option>
-                {/* {opciones.map(opcion =>(
-                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
-                ))} */}
+                {opciones.map(opcion =>(
+                    <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name}>{opcion.CoinInfo.FullName}</option>
+                ))}
             </Select>
         </Fragment>
     );
